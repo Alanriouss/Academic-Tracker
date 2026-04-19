61b906865531367bfa65ef40b89773d4cfbbf9c2
Ryan <ryananderson2205@gmail.com>
2026-04-19 23:32:36 +0700

    Add files via upload

---
+++ Test/Menu.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public abstract class Menu {
+    protected Scanner scanner = new Scanner(System.in);
+    public abstract void display();
+    public abstract void handleInput();
+}
