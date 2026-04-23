15d1271e53d1e5026f07920f71fe7a5c27cff282
Alan <huynhbdung207@gmail.com>
2026-04-23 10:01:59 +0700

    something

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -1,6 +1,7 @@
 package tracker;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Academic Tracker is booting up!");
+        System.out.println("Hello World!");
     }
 }
--- src/main/java/tracker/Menu.java
+++
@@ -1,7 +0,0 @@
-package tracker;
-import java.util.Scanner;
-public abstract class Menu {
-    protected Scanner scanner = new Scanner(System.in);
-    public abstract void display();
-    public abstract void handleInput();
-}
